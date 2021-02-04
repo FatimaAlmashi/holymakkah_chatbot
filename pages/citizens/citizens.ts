c93654e7399331195854f347d95e657dc995a457
FatimaAlmashi <f.almashi@windowslive.com>
2021-02-04 09:48:11 +0300

    2 files modified, 2 added

---
+++ pages/citizens/citizens.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+
+@Component({
+  selector: "page-citizens",
+  templateUrl: "citizens.html"
+})
+export class CitizensPage {
+  constructor(public navCtrl: NavController) {}
+}
